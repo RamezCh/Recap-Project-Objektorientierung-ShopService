8370cc9194d6f5b5372fbdf8c067b12061a930cb
Ramez Chreide <ramiz_chreide@outlook.com>
2025-02-17 14:35:26 +0100

    add order status enum

---
+++ src/main/java/OrderStatus.java
@@ -0,0 +1,16 @@
+public enum OrderStatus {
+    PROCESSING("processing"),
+    IN_DELIVERY("in delivery"),
+    COMPLETED("completed");
+
+    private final String value;
+
+    OrderStatus(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
+
