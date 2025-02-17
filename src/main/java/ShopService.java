611cac6f644924d1f94a3b6943ea352668d3d938
Ramez Chreide <ramiz_chreide@outlook.com>
2025-02-17 15:28:09 +0100

    add: getProductById returns Optional now

--- src/main/java/ShopService.java
+++ src/main/java/ShopService.java
@@ -8,16 +8,13 @@ public class ShopService {
     public Order addOrder(List<String> productIds) {
         List<Product> products = new ArrayList<>();
         for (String productId : productIds) {
-            Product productToOrder = productRepo.getProductById(productId);
-            if (productToOrder == null) {
-                System.out.println("Product mit der Id: " + productId + " konnte nicht bestellt werden!");
-                return null;
-            }
+            Product productToOrder = productRepo.getProductById(productId)
+                    .orElseThrow(() -> new IllegalArgumentException("Product mit der Id: " + productId + " konnte nicht gefunden werden!"));
+
             products.add(productToOrder);
         }
 
         Order newOrder = new Order(UUID.randomUUID().toString(), products, OrderStatus.PROCESSING);
-
         return orderRepo.addOrder(newOrder);
     }
 
--- src/test/java/ProductRepoTest.java
+++ src/test/java/ProductRepoTest.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -22,15 +19,28 @@ class ProductRepoTest {
     }
 
     @org.junit.jupiter.api.Test
-    void getProductById() {
+    void getProductById_returnProduct() {
         //GIVEN
         ProductRepo repo = new ProductRepo();
 
         //WHEN
-        Product actual = repo.getProductById("1");
+        Optional<Product> actual = repo.getProductById("1");
 
         //THEN
         Product expected = new Product("1", "Apfel");
+        assertEquals(actual.get(), expected);
+    }
+
+    @org.junit.jupiter.api.Test
+    void getProductById_returnEmptyOptional() {
+        //GIVEN
+        ProductRepo repo = new ProductRepo();
+
+        //WHEN
+        Optional<Product> actual = repo.getProductById("3");
+
+        //THEN
+        Optional<Product> expected = Optional.empty();
         assertEquals(actual, expected);
     }
 
@@ -46,7 +56,7 @@ class ProductRepoTest {
         //THEN
         Product expected = new Product("2", "Banane");
         assertEquals(actual, expected);
-        assertEquals(repo.getProductById("2"), expected);
+        assertEquals(repo.getProductById("2").get(), expected);
     }
 
     @org.junit.jupiter.api.Test
@@ -58,6 +68,6 @@ class ProductRepoTest {
         repo.removeProduct("1");
 
         //THEN
-        assertNull(repo.getProductById("1"));
+        assertEquals(repo.getProductById("1"), Optional.empty());
     }
 }
